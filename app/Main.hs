64371735d1a9f977a8e5a4c1897ed37169d7d7aa
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-11-23 20:05:03 +0100

    feat: add brick dependency, add example UI

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
 module Main (main) where
 
-import Lib
+import Brick (Widget, defaultMain, simpleMain, (<=>), (<+>), str, withBorderStyle, joinBorders)
+import qualified Brick.Main as BM
+import Brick.Widgets.Center (center)
+import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
+import Brick.Widgets.Border.Style (unicode)
+
+data State =
+    State { text :: String }
+    deriving (Show)
+
+ui :: State -> Widget ()
+ui state =
+    joinBorders $
+    withBorderStyle unicode $
+    borderWithLabel (str (text state)) $
+    (center 
+        (center 
+            (str "Left")
+            <=> hBorder
+            <=> center (str "Bottom Left")
+        ) 
+        <+> vBorder 
+        <+> center (
+            str "Right"
+        )
+    )
 
 main :: IO ()
-main = someFunc
+main = do
+    {- let app = BM.App {
+        BM.appDraw = \s -> ui s
+    }
+        initialState = State { text = "Default Text" }
+
+    finalState <- defaultMain app initialState
+
+    putStrLn finalState -}
+
+    let initialState = State { text = "Default Text" } in simpleMain (ui initialState)
